ded0f31ba7575a4fd52d1d25bfcda2ebf094e24f
Gastón Ferreyra <gaston@ohana.digital>
2021-02-02 22:47:03 -0300

    creada pantalla de ver noticia

---
+++ screens/ViewNews.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const ViewNews = () => {
+  return (
+    <View>
+      <Text>View news</Text>
+    </View>
+  );
+};
+
+export default ViewNews;
+
+const styles = StyleSheet.create({});
