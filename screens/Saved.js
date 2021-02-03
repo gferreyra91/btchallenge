9551b696853a5fed4a40ede479d7d33db7f88ab1
Gastón Ferreyra <gaston@ohana.digital>
2021-02-03 00:05:00 -0300

    listar news guardadas en asyncstorage

--- screens/Saved.js
+++ screens/Saved.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import NewsList from '../components/NewsList';
+import {getNews} from '../db';
 
-const Saved = () => {
+const Saved = ({navigation}) => {
+  const [news, setNews] = useState([]);
+
+  /* on mount fetch news saved on async storage */
+  useEffect(() => {
+    async function onMount() {
+      const newsList = await getNews();
+      setNews(newsList);
+    }
+    onMount();
+  }, [news]);
+
+  if (news.length === 0) {
+    return <Text>No saved news</Text>;
+  }
+
   return (
-    <View>
-      <Text>Saved</Text>
-    </View>
+    <NewsList
+      news={news}
+      onPress={(news) =>
+        navigation.navigate('ViewNews', {
+          news,
+          saveNews: () => saveNews(news),
+        })
+      }
+    />
   );
 };
 
