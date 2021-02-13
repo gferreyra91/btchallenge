326cd29079d3838841238afa11dd0301929e8cd7
Gastón Ferreyra <gaston@ohana.digital>
2021-02-13 18:39:47 -0300

    agregado safearea para mejor integracion con react native elements

--- App.tsx
+++ App.tsx
@@ -14,14 +14,17 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigator from './navigators/MainNavigator';
 import {ThemeProvider} from 'react-native-elements';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
   return (
-    <ThemeProvider>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider>
+        <NavigationContainer>
+          <MainNavigator />
+        </NavigationContainer>
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 };
 
